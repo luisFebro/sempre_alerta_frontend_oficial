504c0ce48bccfebfac4e01d8d2101008cbeacc71
luisFebro <mr.febro@gmail.com>
2023-11-16 12:23:18 +0700

    sempre_alerta_frontend_v1.12.13-1 | 💎principal: - Painel para produção design e estrutura frontend com cadastro (instituição e usuários com backend) + autenticação e acesso com API e banco | 📢 lançamentos: - Lista em tempo real - Novos modais para exibir popup antes de 'confirmar' ou 'finalizar' um alerta SOS - Nova página de cadastro - (Cod. 2) Todos os formulários com validadores, máscaras para todos os tipos de usuários - (Cod. 3) API CRUD completo com todos os formulários de cadastro - (Cod. 3) Página de acesso com novo google identity authenticator - (Cod. 5) Implementação API de login - (Cod. 5) Gerenciador de acesso e sessão com JWT: páginas privadas e públicas - (Cod. 6) Horário para alertas - (Cod. 6) Cadastro de Instituição com pesquisa de CEP + todas as validações + máscaras. | ⚙️ melhorias: - Atualizações na tela inicial de dados e tema - Restrutura de componentes de layout - (Cod. 4) Botão secundário acesso tradicional; - (Cod. 4) Implementação popup One tap - (Cod. 4) Unificação do versionamento por todo painel - (Cod. 5) Atualização do gerenciador de cache tempo real uify versão 2.0 - (Cod. 5) Segurança do localstorage com checagem de atualização manual com API de checagem 'init' + proteção em todas as rotas JWT + cors - (Cod. 5) Melhorias no cache: indexedDB exclusivo para dados de autentificação e localStorage para dados de sessão - (Cod. 6) atualização root urls para rodar em produção, incluso staging env. |🐞 reparos: - Atualização nome 'userTyp' para 'role' (admin, equipe, autoridade, dev)

--- src/components/_layout/Footer.js
+++ src/components/_layout/Footer.js
@@ -4,11 +4,12 @@ export default function Footer() {
     const { version } = useData();
 
     return (
-        <footer className="mt-96 lg:relative lg:bottom-0 py-6 px-16 border-t border-gray-200 font-light flex flex-col lg:flex-row justify-center items-center max-h-1">
-            <p className="font-normal lg:mb-0">
-                Sempre Alerta &copy; {new Date().getFullYear()} | versão{" "}
-                {version}
-            </p>
+        <footer className="mt-96 lg:relative lg:bottom-0 py-10 px-16 border-t border-gray-200 font-light flex flex-col lg:flex-row justify-center items-center max-h-1">
+            <span className="text-normal text-center m-0 p-0">
+                Sempre Alerta &copy; {new Date().getFullYear()}
+                <br />
+                <span className="text-gray-500">versão {version}</span>
+            </span>
         </footer>
     );
 }
