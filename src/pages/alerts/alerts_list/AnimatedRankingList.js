ad0863859588cf0090612b44a0e082c59645e815
luisFebro <mr.febro@gmail.com>
2023-11-23 02:21:59 +0700

    updating

--- src/pages/alerts/alerts_list/AnimatedRankingList.js
+++ src/pages/alerts/alerts_list/AnimatedRankingList.js
@@ -31,12 +31,12 @@ export default function AnimatedRankingList({ dataList, activeScreenId }) {
         }));
     }, [dbList && dbList.length]);
 
+    sortDatesFront(list, {
+        target: "createdAt",
+    });
+
     useEffect(() => {
         // update list by date when a change happens
-
-        sortDatesFront(list, {
-            target: "createdAt",
-        });
 
         setData((prev) => ({
             ...prev,
