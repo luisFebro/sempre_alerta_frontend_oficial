3b93e09dd7b3f6812f861254d800b5393b71b32a
luisFebro <mr.febro@gmail.com>
2023-12-02 19:37:17 +0700

    updating: página de privacidade

--- src/pages/docs/PrivacyPolicy.js
+++ src/pages/docs/PrivacyPolicy.js
@@ -56,23 +56,125 @@ const PrivacyPolicy = () => {
                 para:
             </p>
 
-            <ul className="text-normal">
-                <li> * Fornecer, operar e manter nosso site</li>
-                <li> * Melhorar, personalizar e expandir nosso site</li>
-                <li> * Compreender e analisar como você usa nosso site</li>
+            <ul className="list-disc ml-8 text-normal">
+                <li>Fornecer, operar e manter nosso site</li>
+                <li>Melhorar, personalizar e expandir nosso site</li>
+                <li>Compreender e analisar como você usa nosso site</li>
                 <li>
-                    * Desenvolver novos produtos, serviços, recursos e
+                    Desenvolver novos produtos, serviços, recursos e
                     funcionalidades
                 </li>
                 <li>
-                    * Comunicar-se com você, diretamente ou por meio de um de
+                    Comunicar-se com você, diretamente ou por meio de um de
                     nossos parceiros, para atendimento ao cliente, fornecer
                     atualizações e outras informações relacionadas ao site, e
                     para fins de marketing e promoção
                 </li>
-                <li>* Enviar e-mails</li>
-                <li>* Encontrar e prevenir fraudes</li>
+                <li>Enviar e-mails</li>
+                <li>Encontrar e prevenir fraudes</li>
             </ul>
+
+            <h2 className="text-xl pt-5 font-bold">
+                Políticas de Privacidade de Terceiros
+            </h2>
+
+            <p className="text-normal">
+                A Política de Privacidade da Sempre Alerta SOS não se aplica a
+                outros anunciantes ou sites. Portanto, aconselhamos que consulte
+                as respectivas Políticas de Privacidade desses servidores de
+                anúncios de terceiros para obter informações mais detalhadas.
+                Isso pode incluir práticas e instruções sobre como optar por não
+                participar de certas opções.
+            </p>
+
+            <p className="text-normal">
+                Você pode optar por desativar cookies através das opções
+                individuais do seu navegador. Para obter informações mais
+                detalhadas sobre o gerenciamento de cookies com navegadores
+                específicos, consulte os sites respectivos dos navegadores.
+            </p>
+
+            <h2 className="text-xl pt-5 font-bold">
+                Direitos de Privacidade do CCPA (Não Venda das Minhas
+                Informações Pessoais)
+            </h2>
+
+            <p className="text-normal">
+                Sob o CCPA, entre outros direitos, os consumidores têm o direito
+                de:
+            </p>
+
+            <ul className="list-disc ml-8">
+                <li className="text-normal">
+                    Solicitar que um negócio que coleta dados pessoais de um
+                    consumidor divulgue as categorias e peças específicas de
+                    dados pessoais coletados sobre os consumidores.
+                </li>
+                <li className="text-normal">
+                    Solicitar que um negócio exclua qualquer dado pessoal sobre
+                    o consumidor que o negócio tenha coletado.
+                </li>
+                <li className="text-normal">
+                    Solicitar que um negócio que vende os dados pessoais de um
+                    consumidor não venda esses dados pessoais do consumidor.
+                </li>
+            </ul>
+
+            <p className="text-normal">
+                Se você fizer uma solicitação, temos um mês para responder. Se
+                desejar exercer qualquer um desses direitos, entre em contato
+                conosco.
+            </p>
+
+            <h2 className="text-xl pt-5 font-bold">
+                Direitos de Proteção de Dados do GDPR
+            </h2>
+
+            <p className="text-normal">
+                Queremos garantir que você esteja plenamente ciente de todos os
+                seus direitos de proteção de dados. Todo usuário tem direito aos
+                seguintes:
+            </p>
+
+            <ul className="list-disc ml-8">
+                <li className="text-normal">
+                    <strong>Direito de Acesso</strong> – Você tem o direito de
+                    solicitar cópias de seus dados pessoais.
+                </li>
+                <li className="text-normal">
+                    <strong>Direito de Retificação</strong> – Você tem o direito
+                    de solicitar que corrijamos qualquer informação que acredita
+                    estar incorreta. Você também tem o direito de solicitar que
+                    completemos as informações que acredita estar incompletas.
+                </li>
+                <li className="text-normal">
+                    <strong>Direito ao Esquecimento</strong> – Você tem o
+                    direito de solicitar que apaguemos seus dados pessoais, sob
+                    certas condições.
+                </li>
+                <li className="text-normal">
+                    <strong>Direito de Restrição de Processamento</strong> –
+                    Você tem o direito de solicitar que restrinjamos o
+                    processamento de seus dados pessoais, sob certas condições.
+                </li>
+                <li className="text-normal">
+                    <strong>Direito de Oposição ao Processamento</strong> – Você
+                    tem o direito de se opor ao nosso processamento de seus
+                    dados pessoais, sob certas condições.
+                </li>
+                <li className="text-normal">
+                    <strong>Direito à Portabilidade de Dados</strong> – Você tem
+                    o direito de solicitar que transfiramos os dados que
+                    coletamos para outra organização ou diretamente para você,
+                    sob certas condições.
+                </li>
+            </ul>
+
+            <p className="text-normal">
+                Se você fizer uma solicitação, temos um mês para responder. Se
+                desejar exercer qualquer um desses direitos, entre em contato
+                conosco.
+            </p>
         </div>
     );
 };
